7a74b9d6a85377c76476cd6e7ac228cc82d14cc3
Himanshu Negi <himanshunegi267@gmail.com>
2023-08-02 11:03:29 +0530

    nav bar completed

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -27,6 +27,7 @@ export const Navbar = ({hamburgerIcons,setHamburgerIcons,menuPos,setMenuPos}) =>
     const hamRef=useRef(null);
     const profileDropDownRef=useRef(null);
     const profile=useRef(null);
+    const [profSlide,setProfSlide]=useState(false);
     const fetchSubLinks=async()=>{
             try{
                 const result=await apiConnector("GET",categories.CATEGORIES_API);
@@ -165,7 +166,7 @@ export const Navbar = ({hamburgerIcons,setHamburgerIcons,menuPos,setMenuPos}) =>
             {
                 hamburgerIcons &&
                 <ul ref={menuRef} className={`flex w-[45%] max-[426px]:w-full min-h-screen flex-col absolute z-40 top-0 right-0 ${menuPos?"translate-x-0 transition-transform duration-[300] ease-out":"translate-x-[100%] transition-transform duration-[300] ease-in"}  bg-richblack-700 pt-8 pb-6 pl-6 pr-6 lg:gap-6 gap-8   text-richblack-25`}>
-                    <div className='flex gap-x-4 items-center h-[20%]'>
+                    <div className='flex gap-x-4 items-center h-[25%] mt-2 ml-2'>
                     {
                         user && user?.accountType!="Instructor" && (
                             <div className='flex gap-8'>
@@ -178,7 +179,19 @@ export const Navbar = ({hamburgerIcons,setHamburgerIcons,menuPos,setMenuPos}) =>
                                     </span>
                                 }
                             </Link>
-                            <img src={`${user.image}`} className='w-[30px] h-[30px] rounded-full'></img>
+                            <div className='flex items-center gap-2 cursor-pointer' onClick={()=>{if(secSlide)
+                                {
+                                    return
+                                }
+                                setProfSlide((prev)=>!prev)}}>
+                                <img src={`${user.image}`} className='w-[30px] h-[30px] rounded-full'></img>
+                                {
+                                    !secSlide &&
+                                    <AiOutlineArrowLeft className={` ${profSlide?"":"rotate-180"}`}/>
+                                    
+                                }
+                            </div>
+                            
                             </div>
                             
                         )
@@ -207,7 +220,7 @@ export const Navbar = ({hamburgerIcons,setHamburgerIcons,menuPos,setMenuPos}) =>
                                             links.title=="Catalog"?(
                                                 <div className='flex items-center gap-1 transition-all duration-200  cursor-pointer' onClick={()=>{setSecSlide((prev)=>!prev);console.log("second slide sliding in")}}>
                                                     {links.title}
-                                                    <AiOutlineArrowLeft className='translate-y-[2px] rotate-180'/>
+                                                    <AiOutlineArrowLeft className={`translate-y-[2px] rotate-180`}/>
                                                 </div>
                                             ):(
                                                 <Link to={links?.path} className={`${matchRoute(links?.path)?"text-yellow-25":""}`}>{links.title}</Link>
@@ -228,6 +241,26 @@ export const Navbar = ({hamburgerIcons,setHamburgerIcons,menuPos,setMenuPos}) =>
                                     })
                                 }
                             </div>
+                            {
+                                user && user?.accountType!="Instructor" &&(
+                                    <div className={`flex gap-8 flex-col h-[75vh] overflow-auto overflow-x-hidden bg-richblack-700 ${profSlide?"translate-x-0 transition-transform duration-[300] ease-out":"translate-x-[120%] transition-transform duration-[300] ease-in"} absolute z-40 w-full`}>
+                                        <div className='flex flex-col gap-8'>
+                                           <Link to="/dashboard">
+                                                <div className='flex gap-4 items-center'>
+                                                    <AiOutlineDashboard size={25}/>
+                                                    <p className='text-lg'>Dashboard</p>
+                                                </div>
+                                           </Link>
+                                           <Link>
+                                                <div className='flex gap-4 items-center'>
+                                                    <HiOutlineLogout size={25}/>
+                                                    <p className='text-lg'>Logout</p>
+                                                </div>
+                                           </Link>
+                                        </div>
+                                    </div>
+                                )
+                            }
                     </div>
                     <AiOutlineClose className='absolute top-[6px] right-[6px]' onClick={()=>{setMenuPos((prev)=>!prev);console.log("menu bar sliding out");console.log(menuPos)}}/>
                     <AiOutlineArrowLeft onClick={()=>{setSecSlide((prev)=>!prev);console.log("sec slide moving out")}} className={`absolute top-[10px] left-[15px] ${secSlide?"visible":"invisible"} cursor-pointer`}/>
@@ -247,23 +280,23 @@ export const Navbar = ({hamburgerIcons,setHamburgerIcons,menuPos,setMenuPos}) =>
                                     </span>
                                 }
                             </Link>
-                            <div ref={profile} className='flex gap-1 items-center cursor-pointer relative' onClick={()=>{setProfileDropDown(true)}}>
+                            <div ref={profile} className='flex gap-1 items-center cursor-pointer relative' onClick={()=>{setProfileDropDown((prev)=>!prev)}}>
                                 <img src={`${user.image}`} className='w-[30px] h-[30px] rounded-full' ></img>
                                 <AiOutlineDown fill='white' className='translate-y-[3px]'/>
                                 {
                                 <div ref={profileDropDownRef} className={`bottom-0 right-[-10px] border-[0.5px] ${profileDropDown?"visible":"invisible"} border-richblack-400/30 h-[120px] rounded-lg  w-[150px] bg-richblack-800 translate-y-[calc(100%_+_13px)] z-50  absolute`}>
                                     <div className='flex flex-col items-center h-full w-full '>
                                         <Link to="/dashboard" >
-                                                <div className='w-[150px] h-[60px] border-b-[1px] p-2 flex gap-3 items-center  border-richblack-500'>
+                                                <div className='w-[150px] h-[60px] border-b-[1px]  hover:bg-richblack-600 rounded-t-md p-2 flex gap-3 items-center  border-richblack-500'>
                                                 <AiOutlineDashboard fill='#AFB2BF' size={25}/>
-                                                <p className='text-richblack-200 text-lg'>Dashboard</p>
+                                                <p className='text-richblack-100 text-lg'>Dashboard</p>
                                                 </div>
                                                 
                                         </Link>
                                         <Link>
-                                                <div className='w-[150px] h-[60px] flex gap-3 p-2 items-center'>
+                                                <div className='w-[150px] h-[60px] flex gap-3 rounded-b-md hover:bg-richblack-600 p-2 items-center'>
                                                 <HiOutlineLogout stroke='#AFB2BF' size={25}/>
-                                                <p className='text-richblack-200 text-lg'>Logout</p>
+                                                <p className='text-richblack-100 text-lg'>Logout</p>
                                                 </div>
                                                 
                                         </Link>
