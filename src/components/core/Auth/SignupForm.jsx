bd49f5c1af8b13467ca8a8c1a7c3bcbcebd6e315
Himanshu Negi <himanshunegi267@gmail.com>
2023-08-08 11:21:21 +0530

    signup feature done

--- src/components/core/Auth/SignupForm.jsx
+++ src/components/core/Auth/SignupForm.jsx
@@ -1,7 +1,136 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {AiOutlineEyeInvisible} from 'react-icons/ai';
+import {AiOutlineEye} from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { sendotp } from '../../../services/operations/authAPI';
+import {setSignupData} from '../../../slices/authSlice';
 export const SignupForm = () => {
+  const {signupData}=useSelector((state)=>state.auth);
+  const [accountType,setAccountType]=useState("Student");
+  const [showPassword,setShowPassword]=useState(false);
+  const [showPassword1,setShowPassword1]=useState(false);
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+  const [signData,setSignData]=useState(
+    {
+      firstName:"",
+      lastName:"",
+      email:"",
+      password:"",
+      confirmPassword:"",
+      accountType:""
+    }
+  );
+
+  const handlePass=(event)=>{
+    if(event.target.getAttribute('name')==="pass1" || event.target.parentNode.getAttribute('name')==="pass1")
+    {
+      setShowPassword1((prev)=>(
+        !prev
+      ))
+    }
+    else if(event.target.getAttribute('name')==="pass2" || event.target.parentNode.getAttribute('name')==="pass2")
+    {
+      setShowPassword((prev)=>(
+        !prev
+      ))
+    }
+  }
+  const handleChange=(event)=>{
+    setSignData((prev)=>({
+      ...prev,
+      [event.target.name]:event.target.value,
+      accountType:accountType
+    }));
+  }
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    dispatch(sendotp(signData.email,navigate));
+    dispatch(setSignupData(signData));
+    setSignData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    })
+  }
   return (
-    <div>SignupForm</div>
+    <div className='flex flex-col mb-9 gap-6 text-white'>
+        <div className='flex max-w-[230px] rounded-full p-1 text-lg bg-richblack-700/50 shadow-sm shadow-richblack-300'>
+          <button onClick={()=>{setAccountType("Student")}} className={`w-[50%] transition-all ease-in duration-[150ms] rounded-full p-2 ${accountType==="Student"?"bg-richblack-900":""}`}>Student</button>
+          <button onClick={()=>{setAccountType("Instructor")}} className={`w-[50%] p-2 transition-all ease-in duration-[150ms] rounded-full ${accountType==="Instructor"?"bg-richblack-900":""}`}>Instructor</button>
+        </div>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+          <div className='flex gap-4'>
+            <div>
+                <label>
+                  <p>
+                    First Name
+                    <sup className='text-pink-200'>*</sup>
+                  </p>
+                </label>
+                <input onChange={handleChange} name='firstName' placeholder='Enter your First Name' type='text' required className='appearance-none focus:outline-none focus:shadow-outline focus:ring-richblack-50 w-full bg-richblack-700 text-richblack-5 rounded-lg py-3 px-3 shadow-md shadow-richblack-300'>
+                </input>
+            </div>
+            <div>
+                <label>
+                  <p>
+                    Last Name
+                    <sup className='text-pink-200'>*</sup>
+                  </p>
+                </label>
+                <input onChange={handleChange} name='lastName' placeholder='Enter your Last Name' type='text' required className='appearance-none focus:outline-none focus:shadow-outline focus:ring-richblack-50 w-full bg-richblack-700 text-richblack-5 rounded-lg py-3 px-3 shadow-md shadow-richblack-300'></input>
+            </div>
+          </div>
+          <div>
+            <label><p>
+              Email Address
+              <sup className='text-pink-200'>*</sup>
+            </p></label>
+            <input onChange={handleChange} name='email' placeholder='Enter Email Address' type='text' required className='appearance-none focus:outline-none focus:shadow-outline focus:ring-richblack-50 w-full bg-richblack-700 text-richblack-5 rounded-lg py-3 px-3 shadow-md shadow-richblack-300'></input>
+          </div>
+          <div className='flex gap-4'>
+            <div className='relative'>
+                <label>
+                  <p>
+                    Create Password
+                    <sup className='text-pink-200'>*</sup>
+                  </p>
+                </label>
+                <input onChange={handleChange} name='password' placeholder='Enter Password' type={`${showPassword1?"text":"password"}`} required className='appearance-none focus:outline-none focus:shadow-outline focus:ring-richblack-50 w-full pr-[36px] bg-richblack-700 text-richblack-5 rounded-lg py-3 pl-3 shadow-md shadow-richblack-300'>
+                </input>
+                {
+                  showPassword1?
+                  <AiOutlineEye onClick={handlePass} name='pass1' className='absolute z-10 bottom-[12px] right-[10px] cursor-pointer' fill='#AFB2BF' size={23}  />:
+                  <AiOutlineEyeInvisible onClick={handlePass} name='pass1' className='absolute z-10 bottom-[12px] right-[10px] cursor-pointer' fill='#AFB2BF' size={23} />
+                }
+            </div>
+            <div className='relative'>
+                <label>
+                  <p>
+                    Confirm Password
+                    <sup className='text-pink-200'>*</sup>
+                  </p>
+                </label>
+                
+                <input onChange={handleChange} name='confirmPassword' placeholder='Confirm Password' type={`${showPassword?"text":"password"}`} required className='appearance-none focus:outline-none focus:shadow-outline focus:ring-richblack-50 w-full pr-[36px] bg-richblack-700 text-richblack-5 rounded-lg py-3 pl-3 shadow-md shadow-richblack-300'></input>
+                {
+                  showPassword?
+                  <AiOutlineEye name='pass2' onClick={handlePass} className='absolute z-10 bottom-[12px] right-[10px] cursor-pointer' fill='#AFB2BF' size={23}  />:
+                  <AiOutlineEyeInvisible name='pass2' onClick={handlePass} className='absolute z-10 bottom-[12px] right-[10px] cursor-pointer' fill='#AFB2BF' size={23} />
+
+                }
+            </div>
+          </div>
+          <button
+              type="submit"
+              className="mt-6 rounded-[8px] bg-yellow-50 py-[8px] px-[12px] font-medium text-richblack-900"
+          >
+              Create An Account
+          </button>
+        </form>
+    </div>
   )
 }
--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -1,13 +1,64 @@
 import { toast } from "react-hot-toast"
-import {setLoading,setToken} from "../../slices/authSlice"
+import {setLoading,setSignupData,setToken} from "../../slices/authSlice"
 import {setUser} from "../../slices/profileSlice"
 import {apiConnector} from '../apiconnector'
 import {endpoints} from '../apis'
 import {resetCart} from '../../slices/cartSlice'
 const {
     LOGIN_API,
-    SIGNUP_API
+    SIGNUP_API,SENDOTP_API
 }=endpoints
+export function sendotp(email,navigate)
+{
+    return async(dispatch)=>{
+        const toastID=toast.loading("Loading...");
+        dispatch(setLoading(true));
+        try{
+            const res=await apiConnector("POST",SENDOTP_API,{
+                email
+            });
+            console.log("SEND OTP",res);
+            if(!res.data.success)
+            {
+                throw new Error(res.data.message)
+            }
+            toast.success("OTP SEND to "+email);
+            navigate("/verify-email");
+        }catch(err)
+        {
+            console.log("SENDOTP API ERROR............", err)
+            toast.error("Signup Failed")
+        }
+        dispatch(setLoading(false));
+        toast.dismiss(toastID);
+    }
+}
+export function signup(email,accountType,firstName,lastName,password,confirmPassword,otp,navigate)
+{
+    return async(dispatch)=>{
+        const toastID=toast.loading("Loading...");
+        dispatch(setLoading(true));
+        try{
+            const res=await apiConnector("POST",SIGNUP_API,{
+                email,accountType,firstName,lastName,password,confirmPassword,otp
+            });
+            console.log("sign up res",res);
+            if(!res.data.success)
+            {
+                throw new Error(res.data.message)
+            }
+            toast.success("Sign up success");
+            setSignupData(null);
+            navigate("/login");
+        }catch(err)
+        {
+            console.log("SIGNUP_ERROR",err);
+            toast.error("SIGNUP FAILED");
+        }
+        dispatch(setLoading(false));
+        toast.dismiss(toastID);
+    }
+}
 export function login(email,password,navigate){
     return async(dispatch)=>{
         const toastID=toast.loading("Loading...")
