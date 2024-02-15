7b92afbdd8350a24a20997a6216401169139d31b
Himanshu Negi <himanshunegi267@gmail.com>
2024-02-15 15:53:41 +0530

    cors changed2

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"https://study-notion-app-frontend.vercel.app/",
+        origin:"https://study-notion-app-fronten-git-e63e67-himanshus-projects-e18e233b.vercel.app",
         credentials:true,
     })
 )
