ad542421622d81935a1c15f8cdfaf66a6eb00404
Himanshu Negi <himanshunegi267@gmail.com>
2024-02-15 16:47:35 +0530

    cors changed to vercel frontend

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"*",
+        origin:"https://study-notion-app-git-master-himanshus-projects-e18e233b.vercel.app/",
         credentials:true,
     })
 )
