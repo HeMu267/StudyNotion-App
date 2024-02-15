65a99757ab038c78fdfe08e83f1c04b6aa51a52e
Himanshu Negi <himanshunegi267@gmail.com>
2024-02-15 15:46:24 +0530

    cors changed 1

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"*",
+        origin:"https://study-notion-app-frontend.vercel.app/",
         credentials:true,
     })
 )
